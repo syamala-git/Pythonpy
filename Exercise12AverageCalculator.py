d833ff60350b582ddb3cb3459f1d9d106f90c9e6
syamala-git <syamala.kovvuri1@gmail.com>
2026-05-12 16:02:09 +0200

    created function average calc

---
+++ Exercise12AverageCalculator.py
@@ -0,0 +1,6 @@
+def CalculateAverage(num1, num2, num3):
+    average = (num1 + num2 + num3) / 3
+    print(f"The average of {num1}, {num2}, and {num3} is {average}")
+
+
+CalculateAverage(10, 20, 30)
